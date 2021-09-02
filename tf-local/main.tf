e78069055e03a4baa3b35f9b3faf69334a7956a6
Your Name <you@example.com>
2021-09-02 17:46:38 +0530

    updated 02-09-05:46

--- tf-local/main.tf
+++ tf-local/main.tf
@@ -25,9 +25,10 @@ provider "docker" {
 
 # It tells which resource to create and consists of resource type and resource name. After execute this resource,
 # it gives ouput(attributes). Please refer the documentation for attribute details or tfstate file. 
+# Lookup is used to retrieve the mapping based on the key value. 
 
 resource "docker_image" "pull_image" {
-  name = "busybox"
+  name = lookup(var.image-mapping, var.environment)
 }
 
 # Referencing the other resources. It consists of resource-type.resource-name.output-of-resource. 
@@ -35,18 +36,24 @@ resource "docker_image" "pull_image" {
 # Port is used to access externally in docker. 
 # var is used to refer the declared variable. 
 # volumes is used to map the host and container port. 
+# path.module / path.root / path.cwd helps to get the file path. Interpolation is used as ${} to update/replace the value. 
+# lookup is to get the value from map by passing key. lookup(map, key). 
+
 
 resource "docker_container" "busybox-container" {
   name  = join("-", ["busybox-container", count.index])
   image = docker_image.pull_image.latest
   count = local.local_count
+  volumes {
+    container_path = "/data"
+    host_path      = "${path.cwd}/hostvolume" #  output will be "/home/meeran/Desktop/terraform/terraform-tutorial/tf-local/hostvolume"
+  }
   command = [
     "sleep",
     "3600"
   ]
   ports {
     internal = var.internal-port
-    external = var.external-port[count.index]
+    external = lookup(var.external-port, var.environment)[count.index]
   }
 }
-
--- tf-local/variables.tf
+++ tf-local/variables.tf
@@ -14,12 +14,15 @@ variable "internal-port" {
 # sensitive is used to hide the value display. NOTE: Still value will be shown in tfstate file. 
 # validation is used to validate the condition. If condition is true then no error. If condition is false, then throw error. 
 # for max function, max([list of values]...) min([list of values]...)  NOTE: ... (spread operator) is required. 
+# in max validation, it can check list only. So to pass the key value of map. 
+# For Prod validation, require to pass as max(list of map[key])
 
 
 variable "external-port" {
-  type = list(any)
+  type = map(any)
+
   validation {
-    condition     = max(var.external-port...) < 2000 && min(var.external-port...) > 1000
+    condition     = max(var.external-port["dev"]...) < 2000 && min(var.external-port["dev"]...) > 1000
     error_message = "Validation is failed with invalid port."
   }
 }
@@ -32,6 +35,26 @@ variable "container_count" {
 # Locals are used to assign a name to expression. Below is the local declaration. 
 # It is referred by using local.local-name. 
 
+
 locals {
-  local_count = length(var.external-port)
+  local_count = length(lookup(var.external-port, var.environment))
+}
+
+
+variable "environment" {
+  type        = string
+  default     = "dev"
+  description = "environement variables"
+}
+
+# Mapping is used to map based on the key/value pair. 
+# Lookup is lookup(map,key,default). It returns the mapping based on the key value. 
+
+variable "image-mapping" {
+  type        = map(any)
+  description = "image mapping example"
+  default = {
+    dev  = "busybox"
+    prod = "ubuntu"
+  }
 }
